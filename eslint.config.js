e1a881e7ccb7a3491b3c134ef60e408b8abd2df3
pwyq <y433wu@edu.uwaterloo.ca>
2025-09-06 19:09:45 -0600

    clean

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import pluginVue from 'eslint-plugin-vue'
-
-export default [
-  { languageOptions: { globals: globals.browser } },
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential']
-]
